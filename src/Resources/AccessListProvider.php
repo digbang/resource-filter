32a124098fad6ffc9082489d1e2710b2176a762d
Dario Govergun <dpgover@gmail.com>
2020-01-14 11:40:11 -0300

    - [UNTESTED] Allow for multiple AggregatorResource's (This will probably not work if the aggregated resources relate to multiple aggregators at the same time) - The AccessListProvider now gets the target entity metadata

--- src/Resources/AccessListProvider.php
+++ src/Resources/AccessListProvider.php
@@ -2,9 +2,10 @@
 
 namespace Digbang\ResourceFilter\Resources;
 
+use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\ORM\QueryBuilder;
 
 interface AccessListProvider
 {
-    public function buildAccessList($userId): QueryBuilder;
+    public function buildAccessList(ClassMetadata $targetEntityMetadata, $userId): QueryBuilder;
 }
